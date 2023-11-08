04a4e3c903854547db3803341a4f1ca1ea4532b3
아기코쟁이 <90755590+haansohee@users.noreply.github.com>
2023-11-08 11:20:03 +0900

    feature: 회원가입 View

--- BookBuddy/Member/ViewController/MemberLoginViewController.swift
+++ BookBuddy/Member/ViewController/MemberLoginViewController.swift
@@ -7,15 +7,20 @@
 
 import Foundation
 import UIKit
+import AuthenticationServices
+import RxSwift
+import RxCocoa
 
 final class MemberLoginViewController: UIViewController {
     private let memberLoginView = MemberLoginView()
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
         configureMemberLoginView()
         setLayoutConstraintsMemberLoginView()
+        bindSignupButton()
     }
 }
 
@@ -32,5 +37,13 @@ extension MemberLoginViewController {
             memberLoginView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
             memberLoginView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
         ])
+    }
+    
+    private func bindSignupButton() {
+        memberLoginView.startToEmailButton.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                self?.present(MemberSignupWithEmailViewController(), animated: true)
+            })
+            .disposed(by: disposeBag)
     }
 }
